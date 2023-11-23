792c276fbf571ee1a43b58c0b4ecd3cda33e8f85
binhkid2 <binhkid2@gmail.com>
2023-11-24 03:16:26 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    
+    import axios from "axios"
+    let aiGene:string = ""
+    let prompt =  "Write a story about a magic backpack"
+function sendApi(){
+    axios.post("https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyCV0ZdEJwEZdPbRhgEQ_ja8UheaoOfTVTA", {
+        "prompt": { "text": prompt} 
+  })
+  .then(function (response) {
+    aiGene = response.data.candidates[0].output
+    console.log(response.data.candidates)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+</script>
+<form on:submit|preventDefault={sendApi}>
+
+<input bind:value={prompt} />
+<button type="submit">Send</button>
+</form>
+<p>{aiGene}</p>
